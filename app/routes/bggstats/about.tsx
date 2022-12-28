44097dfa3a6170028e95defdfd2b443153af3b44
Clayton Ingalls <github@zapbampow.com>
2022-12-28 10:44:38 -0500

    more attempts to fix production build

--- app/routes/bggstats/about.tsx
+++ app/routes/bggstats/about.tsx
@@ -1,7 +1,7 @@
 import type { LinksFunction } from "@remix-run/node";
 import React from "react";
 import { Container } from "~/components/bggStats/pages/layout";
-import Markdown from "../../content/bggStats/about.mdx";
+import Markdown from "~/content/bggStats/about.mdx";
 import styles from "~/styles/bggStats/markdownStyles.css";
 
 export const links: LinksFunction = () => {
@@ -9,8 +9,9 @@ export const links: LinksFunction = () => {
 };
 
 export default function about() {
+  console.log("markdown", Markdown);
   return (
-    <div className="w-full lg:w-[764px] px-2 mx-auto">
+    <div className="w-full lg:w-[764px] px-2 mx-auto pb-8">
       <div className="p-8 mb-16 rounded-md markdown bg-slate-100">
         <Markdown />
       </div>
--- app/routes/bggstats/how-to-use.tsx
+++ app/routes/bggstats/how-to-use.tsx
@@ -1,7 +1,7 @@
 import type { LinksFunction } from "@remix-run/node";
 import React from "react";
 import { Container } from "~/components/bggStats/pages/layout";
-import Markdown from "../../content/bggStats/how-to-use.mdx";
+import Markdown from "~/content/bggStats/how-to-use.mdx";
 import styles from "~/styles/bggStats/markdownStyles.css";
 
 export const links: LinksFunction = () => {
@@ -10,7 +10,7 @@ export const links: LinksFunction = () => {
 
 export default function about() {
   return (
-    <div className="w-full lg:w-[764px] px-2 mx-auto">
+    <div className="w-full lg:w-[764px] px-2 mx-auto pb-8">
       <div className="p-8 mb-16 rounded-md markdown bg-slate-100">
         <Markdown />
       </div>
